154874308f306e4ba0368ab558a8dc744f4b5e7a
Mitchell Foote <mcfoote321@gmail.com>
2022-10-19 20:20:21 -0400

    fixed solution

--- topKFrequentWords.cpp
+++ topKFrequentWords.cpp
@@ -1,42 +1,60 @@
 #include<string>
 #include<vector>
-#include<map>
+#include<unordered_map>
 #include<algorithm>
 #include<cmath>
+#include<queue>
 
 using std::string;
 using std::vector;
-using std::map;
+using std::unordered_map;
+using std::priority_queue;
 using std::pair;
 using std::sort;
 using std::min;
 
 class Solution {
+
+private:
+
+    struct Compare {
+        Compare() {}
+        ~Compare() {}
+
+        bool operator()(const pair<string,int>& p1, const pair<string,int>& p2) {
+
+            return p1.second > p2.second || (p1.second == p2.second && p1.first < p2.first);
+
+        }
+
+    };
+
 public:
 
     vector<string> topKFrequent(vector<string>& words, int k) {
 
-        int n = words.size();
-        map<string, int> frequency;
+        unordered_map<string, int> um;
 
-        for(int i = 0; i < n; i++) {
-            frequency[words[i]]++;
+        for(const string& s:words) um[s]++;
+
+        priority_queue<pair<string, int>, vector<pair<string, int>>, Compare> pq;
+
+        for(const auto& a : um) {
+
+            pq.push(a);
+            if(pq.size() > k) pq.pop();
+
         }
 
-        vector<pair<string, int>> result;
-        vector<string> answer;
-
-        for(auto iter : frequency) {
-            result.push_back({iter.first, iter.second});
+        vector<string> result;
+        while(!pq.empty()) {
+            result.push_back(pq.top().first);
+            pq.pop();
         }
 
-        sort(result.begin(), result.end());
+        reverse(result.begin(), result.end());
 
-        for(int i = 0; i < min(k, n); i++) {
-            answer.push_back(result[i].first);
-        }
-        
-        return answer;
-        
+        return result;
+
     }
 };
