d015b6651dacdef693d954d0e4a4c4035f653e4f
Mitchell Foote <mcfoote321@gmail.com>
2022-11-08 00:09:20 -0500

    new solutions

--- makeTheStringGreat.cpp
+++ makeTheStringGreat.cpp
@@ -6,5 +6,18 @@ class Solution {
 public:
     string makeGood(string s) {
         
+        string answer;
+
+        for(int i = 0; i < s.size(); i++) {
+            answer.push_back(s[i]);
+
+            while(answer.size() && (answer.back()==s[i+1]+32 || answer.back()==s[i+1]-32)) {
+                answer.pop_back();
+                i++;
+            }
+        }
+
+        return answer;
+
     }
 };
