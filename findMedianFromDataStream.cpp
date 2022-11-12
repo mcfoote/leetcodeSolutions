2decee2a49a4d86d0f750d4c0643c273d89c7176
Mitchell Foote <mcfoote321@gmail.com>
2022-11-11 21:50:11 -0500

    new solutions

--- findMedianFromDataStream.cpp
+++ findMedianFromDataStream.cpp
@@ -1,5 +1,16 @@
+#include<queue>
+#include<vector>
+
+using std::priority_queue;
+using std::vector;
+using std::greater;
+
 class MedianFinder {
 public:
+
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
     MedianFinder() {
         
     }
