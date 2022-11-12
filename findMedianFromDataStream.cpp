69beef52c92a5741bbeac7d47513543456e9674e
Mitchell Foote <mcfoote321@gmail.com>
2022-11-11 21:58:05 -0500

    new solutions

--- findMedianFromDataStream.cpp
+++ findMedianFromDataStream.cpp
@@ -16,10 +16,25 @@ public:
     }
     
     void addNum(int num) {
+
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+
+        if(minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
         
     }
     
     double findMedian() {
+
+        if(maxHeap.size() > minHeap.size()) {
+            return maxHeap.top();
+        }
+
+        return (maxHeap.top() + minHeap.top())/2.0;
         
     }
 };
