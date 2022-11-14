25d17d35eb242d820235589468aa2489a4c72473
Mitchell Foote <mcfoote321@gmail.com>
2022-11-13 23:37:28 -0500

    new solutions

--- mostStonesRemoved.cpp
+++ mostStonesRemoved.cpp
@@ -6,11 +6,29 @@ class Solution {
 public:
     int a;
 
-    void dfSearch(){
+    void dfSearch(vector<vector<int>>& stones, int index, vector<bool>& visited){
+        visited[index] = true;
 
+        for(int i = 0; i < a; i++){
+            if(!visited[i] && ((stones[i][0] == stones[index][0]) || (stones[i][1] == stones[index][1]))) {
+                dfSearch(stones, i, visited);
+            }
+        }
     }
-    
+
     int removeStones(vector<vector<int>>& stones) {
+        a = stones.size();
+
+        vector<bool> visited(a, false);
         
+        int count = 0;
+        for(int i = 0; i<a; i++) {
+            if(visited[i])
+                continue;
+            dfSearch(stones, i, visited);
+            count++;
+        }
+        
+        return a - count;
     }
 };
