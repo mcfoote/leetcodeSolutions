bd13472479813948c939fde96eb5d57a7b1350e2
Mitchell Foote <mcfoote321@gmail.com>
2022-11-15 21:42:00 -0500

    new solutions

--- guessNumber.cpp
+++ guessNumber.cpp
@@ -10,6 +10,25 @@
 class Solution {
 public:
     int guessNumber(int n) {
+
+        int start = 1;
+        int end = n;
+        int mid;
+
+        while(start <= end) {
+            mid = start + (end - start)/2;
+            int resolution = guessNumber(mid);
+            if(resolution == 0) {
+                return mid;
+            }else if(resolution == -1) {
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+
+        }
+        
+        return -1;
         
     }
 };
