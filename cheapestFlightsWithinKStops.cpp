02cee7defdfa191c51c21113ff038e2cac6f8b59
Mitchell Foote <mcfoote321@gmail.com>
2023-01-25 20:32:17 -0500

    new sols

---
+++ cheapestFlightsWithinKStops.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+#include<queue>
+
+using std::vector;
+using std::queue;
+
+class Solution {
+public:
+
+    vector<vector<pair<int,int>>> graph;
+    vector<int> dis;
+
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+    }
+};
