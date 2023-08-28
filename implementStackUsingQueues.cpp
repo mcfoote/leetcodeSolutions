0e48f17e9538715c4825cfeeac37b474f52ab100
Mitch Foote <hal-10000@Mitchs-MacBook-Pro.local>
2023-08-28 18:53:36 -0400

    new sol

--- implementStackUsingQueues.cpp
+++ implementStackUsingQueues.cpp
@@ -1,23 +1,47 @@
+#include<queue>
+
+using std::queue;
+
 class MyStack {
 public:
+queue<int> q1;
     MyStack() {
         
     }
     
     void push(int x) {
         
+        q1.push(x);
+
+        int a = q1.size() -1;
+        int i = 0;
+
+        while(i < a) {
+            q1.push(q1.front());
+            i++;
+            q1.pop();
+        }
+
     }
     
     int pop() {
-        
+
+        int b = q1.front();
+        q1.pop();
+        return b;
+
     }
     
     int top() {
-        
+
+        return q1.front();
+
     }
     
     bool empty() {
         
+        return q1.size() == 0;
+
     }
 };
 
