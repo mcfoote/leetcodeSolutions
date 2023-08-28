05cee4305384c8df12655e2fbe17c0f4dcb97b3a
Mitch Foote <hal-10000@Mitchs-MacBook-Pro.local>
2023-08-27 23:16:11 -0400

    new sol

--- maximumLengthOfPairChain.cpp
+++ maximumLengthOfPairChain.cpp
@@ -1,6 +1,30 @@
+#include<vector>
+
+using std::vector;
+
 class Solution {
 public:
+    static bool comparison(vector<int> &vectA, vector<int> &vectB) {
+
+        return vectA[1] < vectB[1];
+
+    }
+
     int findLongestChain(vector<vector<int>>& pairs) {
-        
+
+        sort(pairs.begin(), pairs.end(), comparison);
+
+        int prev = 0; int res = 1;
+
+        for(int i = 1; i < pairs.size(); i++) {
+            if(pairs[prev] < pairs[i]) {
+                prev = i;
+                res++;
+            }
+
+        }
+
+        return res;
+
     }
 };
